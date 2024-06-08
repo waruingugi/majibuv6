003336f40cc9e5c5631b96d88a6e173e17d7cb4a
waruingugi <waruingugientp@gmail.com>
2024-06-08 22:12:30 +0300

    Created Users model

---
+++ commons/models.py
@@ -0,0 +1,18 @@
+import uuid
+
+from django.db import models
+
+
+class Base(models.Model):
+    """
+    All other models inherit from Base.
+    Base contains common fields among the models.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("-created_at",)
--- majibu/settings.py
+++ majibu/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "phonenumber_field",
+    # Apps
+    "users",
+    "commons",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
