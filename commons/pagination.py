6773ba5d9d75246774fb2a32d730ce179d821c15
waruingugi <waruingugientp@gmail.com>
2024-06-21 20:01:13 +0300

    Add pagination

--- commons/pagination.py
+++ commons/pagination.py
@@ -7,3 +7,4 @@ class StandardPageNumberPagination(pagination.PageNumberPagination):
     page_size: int = int(os.environ["PAGE_SIZE"])
     max_page_size: int = int(os.environ["MAXIMUM_PAGE_SIZE"])
     page_query_param: str = "page"
+    page_size_query_param = "page_size"
