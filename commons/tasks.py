518abd9139a75a34f464a3f0c00dbc53048cf5d9
waruingugi <waruingugientp@gmail.com>
2024-07-17 14:52:13 +0300

    Add push delay task

--- commons/tasks.py
+++ commons/tasks.py
@@ -1,13 +1,28 @@
 from celery import shared_task
 
-from commons.constants import SMS_PROVIDERS
+from commons.constants import PUSH_PROVIDERS, SMS_PROVIDERS
+from commons.raw_logger import logger
 
 
 @shared_task  # type: ignore
 def send_sms(*, phone_number: str, type: str, message: str) -> bool:
+    logger.info("Sending sms in background...")
     for provider in SMS_PROVIDERS:
         if provider.send_sms(phone_number, type, message):
             return True
 
     # Raise error if all providers failed here
+    logger.error("Failed to send sms in the background")
+    return False
+
+
+@shared_task  # type: ignore
+def send_push(*, type: str, title: str, message: str, user_id: str) -> bool:
+    logger.info("Sending push notification in background...")
+    for provider in PUSH_PROVIDERS:
+        if provider.send_push(type=type, title=title, message=message, user_id=user_id):
+            return True
+
+    # Raise error if all providers failed here
+    logger.error("Failed to send push notification in the background")
     return False
--- notifications/push.py
+++ notifications/push.py
@@ -1,10 +1,13 @@
 import requests
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import status
 
 from commons.raw_logger import logger
 from notifications.constants import NotificationChannels, NotificationProviders
 from notifications.models import Notification
+
+User = get_user_model()
 
 
 class OneSignalPush:
@@ -15,6 +18,7 @@ class OneSignalPush:
             "Content-Type": "application/json",
             "Authorization": f"Basic {settings.ONESIGNAL_API_KEY}",
         }
+        self.user = None
         self.push_notification_name = "Majibu In-APP Push"
         self.payload = {
             "app_id": f"{settings.ONESIGNAL_APP_ID}",
@@ -26,36 +30,38 @@ class OneSignalPush:
             "small_icon": "majibu_xs_logo",
         }
 
-    def send_push(
-        self, *, type: str, title: str, message: str, user_ids: list[str]
-    ) -> bool:
+    def send_push(self, *, type: str, title: str, message: str, user_id: str) -> bool:
         """A code snippet in the app identifies users and sends their user_id to Onesignal.
         We then use the user_id to specify who the message is being sent to."""
-        logger.info(f"Sending PUSH notification to {user_ids}")
+        logger.info(f"Sending PUSH notification to {user_id}")
         self.payload["contents"] = {"en": message}
         self.payload["headgins"] = {"en": title}
-        self.payload["include_external_user_ids"] = user_ids
+        self.payload["include_external_user_ids"] = user_id
 
         try:
+            user = User.objects.filter(id=user_id).first()
+            self.user = user if user else self.user
+
             notification_obj = Notification.objects.create(
                 type=type,
                 message=message,
                 channel=NotificationChannels.PUSH.value,
                 provider=NotificationProviders.ONESIGNAL.value,
-                receiving_party=str([user_ids]),
+                receiving_party=user_id,
+                user=self.user,
             )
 
             response = requests.post(self.url, json=self.payload, headers=self.headers)
             notification_obj.objects.update(external_response=response.json())
 
             if response.status_code == status.HTTP_200_OK:
-                logger.info(f"Push notification to {user_ids} sent successfully.")
+                logger.info(f"Push notification to {user_id} sent successfully.")
                 return True
 
             return False
 
         except Exception as e:
-            logger.error(f"Exceptioon occured while sending push to {user_ids}: {e}")
+            logger.error(f"Exception occured while sending push to {user_id}: {e}")
             return False
 
 
